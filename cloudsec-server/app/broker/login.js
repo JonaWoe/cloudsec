2cae56c72c8d76bd8ebbd2fe41d56bfec80ad64c
JonaWoe <jonawoessner@gmail.com>
2018-11-22 15:31:32 +0100

    add roles and more users

--- cloudsec-server/app/broker/login.js
+++ cloudsec-server/app/broker/login.js
@@ -1,13 +1,56 @@
 const test_username = 'admin';
 const test_password = 'admin';
 
+const users = [
+    {
+        username: 'admin',
+        password:'admin',
+        role: '1'
+    },
+    {
+        username: 'jona',
+        password:'admin',
+        role: '1'
+    },
+    {
+        username: 'daniel',
+        password:'admin',
+        role: '1'
+    },
+    {
+        username: 'chris',
+        password:'admin',
+        role: '2'
+    },
+    {
+        username: 'fabian',
+        password:'admin',
+        role: '2'
+    },
+    {
+        username: 'derandere',
+        password:'admin',
+        role: '2'
+    },
+
+]
+
 module.exports = {
     login: function (username, password) {
-        if (test_username === username && test_password === password) {
-            return true;
-        } else {
-            return false;
-        }
+
+        var res = {
+            username: '',
+            role: 0
+        };
+
+        users.forEach(user => {
+            if (user.username === username && user.password === password) {
+                res.username = user.username;
+                res.role = user.role;
+            }
+        })
+
+        return res;   
     }
 };
 
